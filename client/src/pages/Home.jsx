4b4430db4af85079dcdd596e41031fe791301403
KashifKhn <kashifkhnx04@gmail.com>
2024-06-05 01:17:51 +0500

    refactor: add support of dark mode to main layout
    Update MainLayout and Home components to wrap content in a div element

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,13 @@ const Home = () => {
     if (!currentWeatherData) return <div>Loading...</div>;
 
   return (
-    <>
+    <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1rem",
+    }}>
       <TimeDate
         darkMode={darkMode}
         currentWeatherData={currentWeatherData}
@@ -21,7 +27,7 @@ const Home = () => {
         setDegUnit={setDegUnit}
         currentWeatherData={currentWeatherData}
       />
-    </>
+    </div>
   );
 };
 
